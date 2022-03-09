151e78406543d9a76c4551de8da0635e07a169db
neko2048 <show.999991@gmail.com>
2022-03-09 22:04:00 +0800

    continuous Age modify

--- src/dataGenerator.py
+++ src/dataGenerator.py
@@ -11,21 +11,54 @@ class DataGenerator(object):
     def getCoefficient(self, candidate):
         return np.random.choice(a=candidate, size=1)[0]
 
-    def getSex(self, treatment):
-        sex = np.full(shape=treatment.shape, fill_value=-1)
-        for i in range(0, len(treatment), 2):
-            sex[i] = 1
+    def getSex(self, age):
+        sex = np.full(shape=age.shape, fill_value=-1)
+        uniqueAge = np.unique(age)
+        for uniqueValue in uniqueAge:
+            uniqueIdx = np.where(age == uniqueValue)
+            count = 0
+            while count < int(len(age)/25/2):
+                sex[uniqueIdx[0][count]] = 1
+                count += 1
         return sex
 
-    def getTreatment(self, ageType):
+    def getTreatment(self, sex, ageType):
         if ageType == -1:
-            return np.array([-1] * int(self.Nyoung * round(1-self.youngTreatRate, 2)) + \
-                            [1] * int(self.Nyoung * round(self.youngTreatRate, 2)))
+            Ntreat = int(self.Nyoung * round(self.youngTreatRate, 4))
+            Nuntreat = int(self.Nyoung * round(1-self.youngTreatRate, 4))
+
         elif ageType == 1:
-            return np.array([-1] * int(self.Nold * round(1-self.oldTreatRate, 2)) + \
-                            [1] * int(self.Nold * round(self.oldTreatRate, 2)))
+            Ntreat = int(self.Nold * round(self.oldTreatRate, 4))
+            Nuntreat = int(self.Nold * round(1-self.oldTreatRate, 4))
+
+        treatment = np.full(shape=(sex.shape), fill_value=-1)
+        count = 0
+        for i in range(len(sex)):
+            if sex[i] == 1:
+                treatment[i] = 1
+                count += 1
+            if count == int(Ntreat/2):
+                break
+        count = 0
+        for i in range(len(sex)):
+            if sex[i] == -1:
+                treatment[i] = 1
+                count += 1
+            if count == int(Ntreat/2):
+                break
+        return treatment
 
     def getAge(self, ageType):
+        if ageType == -1:
+            age = np.array([round(-x/25, 2) for x in range(1, 26)]*int(self.Nyoung/25))
+            np.random.shuffle(age)
+            return age
+        elif ageType == 1:
+            age = np.array([round(x/25, 2) for x in range(1, 26)]*int(self.Nold/25))
+            np.random.shuffle(age)
+            return age
+
+    def getAgeTag(self, ageType):
         if ageType == -1:
             return np.full(self.Nyoung, ageType)
         elif ageType == 1:
